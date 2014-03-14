30b4ce20450439ac52b45fa07c9edd7c6cef00c6
Sri Kadimisetty <s@sri.io>
2014-03-14 06:19:51 -0500

    call new-app just as app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,33 @@
 var map = L.map('map', {
 	center: [46.872201, -96.812393],
 	maxZoom: 17,
-	zoom: 15,
+	zoom: 14,
 	zoomControl: false
 });
 
-L.tileLayer('http://b.tiles.mapbox.com/v3/oslek.map-izae70ik/{z}/{x}/{y}.png').addTo(map);
 
-$.getJSON("http://api.hackfargo.co/calls/type/Peeper?start=6-10-2012&end=3-3-2014",function(data) { 
+// Sri Map 1: sri.hh2oangj
+// Sri Map 2: sri.hh32e6nd
+
+// Shane's Mapbox URL
+// L.tileLayer('http://b.tiles.mapbox.com/v3/oslek.map-izae70ik/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('http://a.tiles.mapbox.com/v3/sri.hh32e6nd/{z}/{x}/{y}.png').addTo(map);
+
+
+$.getJSON("http://api.hackfargo.co/calls/type/Peeper?start=6-10-2012&end=3-13-2014",function(data) { 
 	$.each(data, function(i, item) {
 		var late =item.Lat;
 		var lon = item.Long;
 		var date = item.Meta.DateString;
 		var address = item.Meta.Address;
-		var description = "Peeper reported at " + address + " on " + date;
+		var description = "Peeper reported on " + date + " at " + address;
 		populatePeepers(late, lon, description);
 	});
 });
 
 function populatePeepers(lat, lon, description) {
 	L.circle([lat, lon], 105.7, {
-		color: 'red',
+		color: '',
 		fillColor: '#f03',
 		fillOpacity: 0.5,
 		weight: 1
